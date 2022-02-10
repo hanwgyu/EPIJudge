3fe13da8815061f5237fb7f044a2c529d558151d
Wangyu Han <han.wgyu@gmail.com>
2022-02-10 23:06:30 +0900

    Solve

--- epi_judge_python/rectangle_intersection.py
+++ epi_judge_python/rectangle_intersection.py
@@ -7,8 +7,8 @@ Rect = collections.namedtuple('Rect', ('x', 'y', 'width', 'height'))
 
 
 def intersect_rectangle(r1: Rect, r2: Rect) -> Rect:
-    # TODO - you fill in here.
-    return Rect(0, 0, 0, 0)
+    x1, x2, y1, y2 = max(r1.x, r2.x), min(r1.x+r1.width, r2.x+r2.width), max(r1.y, r2.y), min(r1.y+r1.height, r2.y+r2.height)
+    return Rect(x1, y1, x2-x1, y2-y1) if x1 <= x2 and y1 <= y2 else Rect(0, 0, -1, -1)
 
 
 def intersect_rectangle_wrapper(r1, r2):
